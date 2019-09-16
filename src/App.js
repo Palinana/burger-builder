394f515530d5187b3f77ed68c6ab234f9a293132
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-09-16 16:40:45 -0400

    added app structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import Layout from './components/Layout/Layout';
+
 function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <div>
+            <Layout>
+                <p>data</p>
+            </Layout>
+        </div>
+    );
 }
 
 export default App;
