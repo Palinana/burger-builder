58606afc543b630095d958adbb0b8cd45de5560b
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-09-17 10:51:57 -0400

    added build control components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import Layout from './components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 function App() {
     return (
         <div>
             <Layout>
-                <p>data</p>
+                <BurgerBuilder />
             </Layout>
         </div>
     );
 }
 
 export default App;
+
