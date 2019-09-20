73cb384b49c4acf117e441cc22db367f323658f1
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-09-20 10:06:11 -0400

    created the contact form for the order

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,15 +1,61 @@
 import React, { Component } from 'react';
 
 import Button from '../../../components/UI/Button/Button';
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.module.css';
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        street: '',
-        email: '',
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your name'
+                },
+                value: '',
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                  type: 'text',
+                  placeholder: 'Street'
+                }
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                  type: 'text',
+                  placeholder: 'ZIP CODE'
+                }
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                  type: 'text',
+                  placeholder: 'Country'
+                }
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                  type: 'email',
+                  placeholder: 'Your mail'
+                }
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                  options: [
+                    { value: 'fastest', displayValue: 'Fastest' },
+                    { value: 'cheapest', displayValue: 'Cheapest' }
+                  ]
+                },
+                value: 'fastest',
+            }
+        },
         loading: false
     }
 
@@ -37,11 +83,28 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementsArray = [];
+        for (let key in this.state.orderForm) {
+            formElementsArray.push({
+              id: key,
+              config: this.state.orderForm[key]
+            });
+        }
+
         let form = (
             <form>
-                <input className={classes.Input} type="text" name="name" placeholder="your name"/>
-                <input className={classes.Input} type="text" name="street" placeholder="your street"/>
-                <input className={classes.Input} type="email" name="email" placeholder="your email"/>
+                {formElementsArray.map(formEl => (
+                    <Input
+                        key={formEl.id}
+                        elementType={formEl.config.elementType}
+                        elementConfig={formEl.config.elementConfig}
+                        value={formEl.config.value}
+                        // changed={e => this.inputChangedHandler(e, formEl.id)}
+                        // invalid={!formEl.config.valid}
+                        // shouldValidate={formEl.config.validation}
+                        // touched={formEl.config.touched}
+                    />
+                ))}
                 <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
             </form>
         );
@@ -52,7 +115,7 @@ class ContactData extends Component {
 
         return (
             <div className={classes.ContactData}>
-                <h4>Contact Data Required</h4>
+                <h4>Please Enter Your Contact Data</h4>
                 {form}
             </div>
         )
