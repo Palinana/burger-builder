15c8db25c8546931e5096879fe8465b19db0fbc5
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2019-10-13 11:03:06 -0400

    redirecting users to the checkout page

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -7,6 +7,7 @@ import {
     SET_AUTH_REDIRECT_PATH
 } from './types';
 import axios from 'axios';
+import firebaseAPI from '../../secrets';
 
 // for spinner and loading
 export const authStart = () => {
@@ -31,6 +32,9 @@ export const authFail = error => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: AUTH_LOGOUT
     };
@@ -49,15 +53,24 @@ export const auth = (email, password, isSignup) => {
         dispatch(authStart());
         const authData = {
             email,
-            password
+            password,
+            returnSecureToken: true
         };
-        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDXNR-JzLbBU6crYbggy_GZ14nEpfg9CR0';
+        let url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${firebaseAPI}`;
         if(!isSignup) {
-            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDXNR-JzLbBU6crYbggy_GZ14nEpfg9CR0'
+            url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${firebaseAPI}`
         }
         axios.post(url, authData)
             .then(response => {
                 console.log(response)
+
+                const expirationDate = new Date(
+                    new Date().getTime() + 3600 * 1000
+                );
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.localId);
+
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthTimeout(3600));
             })
@@ -74,3 +87,25 @@ export const setAuthRedirectPath = path => {
         path
     };
 };
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } 
+        else {
+            const expirationDate = new Date(localStorage.getItem('expirationDate'));
+            if (expirationDate <= new Date()) {
+                dispatch(logout());
+            }
+            else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authSuccess(token, userId));
+                dispatch(
+                    checkAuthTimeout(expirationDate.getTime() - new Date().getTime() / 1000)
+                )
+            }
+        }
+    }
+}
--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -23,4 +23,5 @@ export {
     checkAuthTimeout,
     logout,
     setAuthRedirectPath,
+    authCheckState
 } from './auth';
